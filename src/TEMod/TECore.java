5d8f2f0261dbdb57a225408ff9a00fd9638f935e
m1cxzfw3q <mcxzfwq@outlook.com>
2026-08-25 03:35:18 +0800

    test

--- src/TEMod/TECore.java
+++ src/TEMod/TECore.java
@@ -57,10 +57,28 @@ public class TECore extends Mod {
     );
 
     public TECore() {
-        try {
-            LoadRenderer renderer = Reflect.get(Vars.platform.getClass().getSuperclass(), Vars.platform, "loader");
-            if (renderer != null) TEReflect.setStaticFinalField(renderer.getClass(), "color", Color.valueOf("114c7f"));
-        } catch (Exception ignored) {}
+        Reflect.set(Vars.platform.getClass().getSuperclass(), Vars.platform, "loader", new LoadRenderer() {
+            final Color bac = Pal.accent;
+            // 1c95ff
+            {
+                try{
+                    TEReflect.setStaticFinalField(this.getClass(), "color", Color.valueOf("0e4a7f"));
+                    TEReflect.setStaticFinalField(this.getClass(), "orange", "[#" + Color.valueOf("0e4a7f") + "]");
+                } catch (Exception ignored) {}
+            }
+            @Override
+            public void draw() {
+                Pal.accent = Color.valueOf("1C95FF");
+                super.draw();
+                Pal.accent = bac;
+            }
+
+            @Override
+            public void dispose() {
+                super.dispose();
+                Pal.accent = bac;
+            }
+        });
         Events.on(EventType.ClientLoadEvent.class, _e -> {
             if (!OS.isAndroid && OS.javaVersionNumber < 17) {
                 Log.warn("[TEMod] " + Core.bundle.format("misc.temod-low-java-version", OS.javaVersion.split("\\.")[0]));
